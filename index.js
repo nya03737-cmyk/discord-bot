ea9d046dc3cec0f38655219beb24a6482726dacd
nya03737-cmyk <nya03737@gmail.com>
2026-02-05 17:02:18 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
- const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require("discord.js");
 
 const client = new Client({
   intents: [
@@ -43,4 +43,4 @@ client.on("messageCreate", message => {
   }
 });
 
-client.login(process.env.DISCORD_TOKEN); 
+client.login(process.env.DISCORD_TOKEN);
